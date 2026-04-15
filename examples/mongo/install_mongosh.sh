2b337136d1b0dcf14305f2fae9000ab2358298a2
AlxcNL <J.A.Boogaard@hr.nl>
2026-04-15 12:25:20 +0200

    Improved installation script

--- examples/mongo/install_mongosh.sh
+++ examples/mongo/install_mongosh.sh
@@ -4,9 +4,6 @@ sudo apt install -y ca-certificates curl gnupg
 curl -fsSL https://pgp.mongodb.com/server-8.0.asc | sudo gpg --dearmor --yes -o /usr/share/keyrings/mongodb-server-8.0.gpg
 . /etc/os-release
 printf '%s\n' "deb [ arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu $VERSION_CODENAME/mongodb-org/8.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.2.list > /dev/null
-cat /etc/apt/sources.list.d/mongodb-org-8.2.list
-sudo nala update
+sudo apt update
 apt-cache policy mongodb-org
-apt-cache policy mongodb-orgsudo apt install -y mongodb-org
 sudo apt install -y mongodb-org
- 

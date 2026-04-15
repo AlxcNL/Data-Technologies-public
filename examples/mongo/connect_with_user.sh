a191b221024c7164a6f0dfb8661f93920f3c1f5c
AlxcNL <J.A.Boogaard@hr.nl>
2026-04-15 10:45:23 +0200

    Cleaning

--- examples/mongo/connect_with_user.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-MONGODB_URL="mongodb+srv://admin:mongo25@localhost/lyrics?retryWrites=true&w=majority"
-
-mongosh $MONGODB_URL
--- examples/mongo/start_mongo_container.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-docker run -it --name mongod -v ./data:/data/db -d mongo
